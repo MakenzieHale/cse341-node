5fee114e24e1c18d459dee67509a99c5be06f9da
MakenzieHale <makenzielhale@gmail.com>
2021-09-15 15:47:27 -0400

    request handler

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req,res) =>{
-    console.log(req.url,req.method, req.headers);
 
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my node.js server!</h1></body>');
-    res.write('</html>');
-    res.end();
+const routes = require('./routes');
 
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
 
