fc2eb3e5022bcf8314953d7a95cb6c5b1ed6c186
MakenzieHale <makenzielhale@gmail.com>
2021-09-24 12:31:07 -0400

    prove 02

--- prove02/app.js
+++ prove02/app.js
@@ -31,13 +31,13 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes); 
 
 app.use((req,res,next)=>{
-    res.status(404).render('404',{pageTitle:'404 Error' });
+    res.status(404).render('404.ejs',{pageTitle:'404 Error' });
     
 });
 
 
 
 
-app.listen(3000);
+app.listen(5000);
 
 
