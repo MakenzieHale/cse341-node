f1f6106a08090a025273b1d80c045d37faa05683
MakenzieHale <makenzielhale@gmail.com>
2021-09-24 17:04:13 -0400

    organize

--- routes/admin.js
+++ routes/admin.js
@@ -6,19 +6,26 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product',(req,res,next)=>{
-    console.log('In another Middleware!');
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
+    res.render('add-product', {
+        pageTitle: 'Add Product', 
+        path:'/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
     //if sending a response, you don't want to pass next()
 
 });
 
 // /admin/add-product => POST
 router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 
 });
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
