27b31ab0bed81b63fab7826c928d4c2c8538568f
MakenzieHale <makenzielhale@gmail.com>
2021-09-27 19:09:11 -0400

    enhancing app

--- controllers/404.js
+++ controllers/404.js
@@ -1,4 +1,4 @@
 exports.get404Page = (req,res,next)=>{
-    res.status(404).render('404.ejs',{pageTitle:'404 Error' });
+    res.status(404).render('404.ejs',{pageTitle:'404 Error', path:'/404' });
     
 };
