d7e51a37d7d7ec521620d29ebf14df5feb414095
MakenzieHale <makenzielhale@gmail.com>
2021-10-08 17:43:39 -0400

    changes

--- app.js
+++ app.js
@@ -3,32 +3,33 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const errorController = require('./controllers/404');
 const User = require('./models/user');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://makenzieHale:5AETzsTFvEkG2oTy@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
 
 
-
+const app = express();
 
 const corsOptions = {
-    origin: "https:/cseweek2prove.herokuapp.com/",
+    origin: "https://cseweek2prove.herokuapp.com/",
     optionsSuccessStatus: 200
 };
+
 app.use(cors(corsOptions));
 
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+
     family: 4
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://makenzieHale:5AETzsTFvEkG2oTy@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
 
-const app = express();
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
